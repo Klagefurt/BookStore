ed01af2e2496617efd114b3fb834facf2dda24cc
Nikolay Pomogaev <klagefurt@gmail.com>
2024-10-03 00:08:12 +0300

    Add book repository realization in memory

---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "First book"),
+            new Book(2, "Second book"),
+            new Book(3, "Third book")
+        };
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart))
+                        .ToArray();
+        }
+    }
+}
