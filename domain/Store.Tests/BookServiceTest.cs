334f5acef3dcb405d71152f2f4e1b9a660ce5523
Nikolay Pomogaev <klagefurt@gmail.com>
2024-10-08 23:00:59 +0300

    Realize search by ISBN and author

---
+++ domain/Store.Tests/BookServiceTest.cs
@@ -0,0 +1,43 @@
+﻿using Moq;
+
+namespace Store.Tests
+{
+    public class BookServiceTest
+    {
+        [Fact]
+        public void GetAllByQuery_WithIsbn_CallsGetAllByIsbn()
+        {
+            var bookRepositoryStub = new Mock<IBookRepository>();
+            bookRepositoryStub.Setup(x => x.GetAllByIsbn(It.IsAny<string>()))
+                              .Returns(new[] { new Book(1, "", "", "") });
+
+            bookRepositoryStub.Setup(x => x.GetAllByTitleorAuthor(It.IsAny<string>()))
+                              .Returns(new[] { new Book(2, "", "", "") });
+            
+            var bookService = new BookService(bookRepositoryStub.Object);
+
+            var validIsbn = "ISBN 123-456-7890";
+            var actual = bookService.GetAllByQuery(validIsbn);
+
+            Assert.Collection(actual, book => Assert.Equal(1, book.Id));
+        }
+
+        [Fact]
+        public void GetAllByQuery_WithAuthor_CallsGetAllByTitleorAuthor()
+        {
+            var bookRepositoryStub = new Mock<IBookRepository>();
+            bookRepositoryStub.Setup(x => x.GetAllByTitleorAuthor(It.IsAny<string>()))
+                              .Returns(new[] { new Book(1, "", "", "") });
+
+            bookRepositoryStub.Setup(x => x.GetAllByTitleorAuthor(It.IsAny<string>()))
+                              .Returns(new[] { new Book(2, "", "", "") });
+
+            var bookService = new BookService(bookRepositoryStub.Object);
+
+            var invalidIsbn = "123-456-7890";
+            var actual = bookService.GetAllByQuery(invalidIsbn);
+
+            Assert.Collection(actual, book => Assert.Equal(2, book.Id));
+        }
+    }
+}
--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -4,13 +4,21 @@
     {
         private readonly Book[] books = new[]
         {
-            new Book(1, "First book"),
-            new Book(2, "Second book"),
-            new Book(3, "Third book")
+            new Book(1, "ISBN 12342-32121", "H. Martin", "First book"),
+            new Book(2, "ISBN 12342-34421", "J. Smith", "Second book"),
+            new Book(3, "ISBN 12672-39421", "S. Juriev", "Third book")
         };
-        public Book[] GetAllByTitle(string titlePart)
+
+        public Book[] GetAllByIsbn(string query)
         {
-            return books.Where(book => book.Title.Contains(titlePart))
+            return books.Where(book => book.Isbn == query)
+                        .ToArray();
+        }
+
+        public Book[] GetAllByTitleorAuthor(string query)
+        {
+            return books.Where(book => book.Title.Contains(query)
+                                    || book.Author.Contains(query))
                         .ToArray();
         }
     }
